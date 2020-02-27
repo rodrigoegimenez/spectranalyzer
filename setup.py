6834976ab192a149982a8011ecb55b9e3fae6e1d
Rodrigo Gimenez <rodrigog83@gmail.com>
2020-02-27 22:38:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='SpectrAnalyzer',
       version='0.0.1',
       description='Perform Analysis and Fitting of Spectra',
       author='Rodrigo E. Gimenez',
-      packages=['spectranalyzer'])
+      packages=['spectranalyzer'],
+      install_requires=['lmfit'])
